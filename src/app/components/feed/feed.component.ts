374ff9037f3d284d15de7670e9f7d3659acffc89
shir <torushojiro@gmail.com>
2022-03-14 14:58:15 +0200

    capture button works

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Subject, Observable} from "rxjs";
+import {WebcamImage, WebcamInitError} from 'ngx-webcam';
 
 @Component({
   selector: 'app-feed',
@@ -6,12 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
-  //public deviceId: string;
+  // toggle webcam on/off
+  public showWebcam = true;
+
+  //error
+  public errors: WebcamInitError[] = [];
+
   // latest snapshot
-  //public webcamImage: WebcamImage = null;
+  //! for required type (? for optional)
+  public webcamImage: WebcamImage = null !;
 
+  // webcam snapshot trigger
+  private trigger: Subject<void> = new Subject<void>();
 
-  constructor() { }
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info('received webcam image', webcamImage);
+    //save image to database / compare image with database and reroute
+    this.webcamImage = webcamImage;
+  }
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+  public toggleWebcam(): void {
+    this.showWebcam = !this.showWebcam;
+  }
+
+  constructor() { } // if there is a problem with webcam image you should prob init webcam image here
 
   ngOnInit(): void {
   }
