0e1091ee38ce569bebd34875a3240d24270ec572
Eliraz-Ido <Eliraz245@gmail.com>
2022-09-22 01:06:08 +0300

    Adding mongoose, connecting to the db and converting specific strings to general variable.

--- server/server.ts
+++ server/server.ts
@@ -1,30 +1,38 @@
 
 const express = require('express')
 const app = express();
+const http = require('http')
+
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const env = require('../database/env.json')
 
 const routes = require('./routes.ts')
-const bodyParser = require('body-parser')
+
+const server = http.createServer(app);
 
 // Allow any method from any host and log requests
 app.use((req, res , next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-  if('OPTIONS' === req.method) {
-    res.sendStatus(401);
-  } else {
-    console.log(`${req.ip} ${req.method} ${req.url}`);
+  // if( 'OPTIONS' === req.method ) {
+  //   res.sendStatus(401);
+  // } else {
+    console.log( `${ req.ip } ${ req.method } ${ req.url }` );
     next();
-  }
+  // }
 })
 
-// Handle POST requests that come in formatted as JSON
-app.use(express.json())
-app.use('/', routes);
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+let database = mongoose.connect( `mongodb+srv://${ env.MONGODB_USERNAME }:${ env.MONGODB_PASSWORD }@facerecognition.mb7rb0o.mongodb.net/?retryWrites=true&w=majority` ,
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then( () => { console.log('Database is connected') })
+  .catch( err => { console.log('Can not connect to the database'+ err) });
 
-// start our server on port 4201
-app.listen(4201, '127.0.0.1', function() {
-  console.log("Server now listening on 4201");
+app.use( express.json() )
+app.use('/', routes);
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({ extended: true }) );
+server.listen( env.SERVER_PORT, function() {
+  console.log( "Server now listening on "+ env.SERVER_PORT );
 });
