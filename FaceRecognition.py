3c7cf50e35e556d2ed8e3d118b6991faa8aae285
Eliraz-Ido <Eliraz245@gmail.com>
2022-09-28 01:17:52 +0300

    delete unused files

--- FaceRecognition.py
+++
@@ -1,9 +0,0 @@
-import face_recognition
-
-def face_encoding(image){
-  return face_recognition.api.face_encodings(
-    image,
-    known_face_locations=None,
-    num_jitters=1,
-    model='large')
-}
